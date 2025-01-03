e026b45de592aecbce496fd321b09b9b3ab246db
BobiButt <118150902+BobiButt@users.noreply.github.com>
2025-01-03 21:10:18 +0500

    travel agency website demo
    demo

---
+++ js/burger.js
@@ -0,0 +1,19 @@
+
+    // const burger = document.querySelector('.burger');
+    // const nav = document.querySelector('.nav-links');
+
+    // burger.addEventListener('click', () => {
+    //   // Toggle the nav-active class
+    //   nav.classList.toggle('nav-active');
+
+    //   Toggle the burger animation
+    //   burger.classList.toggle('active');
+    // });
+    function burger() {
+        const sd = document.getElementById('sd');
+        if (sd) {
+        // alert("i am working");
+            
+        }
+        sd.classList.toggle('SDactive'); // Toggle the active class
+      }
